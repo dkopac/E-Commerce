e4f32f6f72f15b04856b16d482fdf5873169c461
[Dan <123705636+dkopac@users.noreply.github.com>
2023-05-21 07:39:38 -0400

    setup category routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -1,28 +1,65 @@
-const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  Category.findAll({
+    include: Product,
+  })
+    .then((categories) => {
+      res.json(categories);
+    })
+    .catch((err) => res.status.json({ "message: Internal server error": err }));
 });
 
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: { id: req.params.id },
+    include: Product,
+  })
+    .then((category) => {
+      res.status(200).json(category);
+    })
+    .catch((err) =>
+      res.status(500).json({ "message: Internal server error": err })
+    );
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+router.post("/", (req, res) => {
+  Category.create(req.body)
+    .then((category) => {
+      res.status(200).json(category);
+    })
+    .catch((err) =>
+      res.status(500).json({ "message: Internal server error": err })
+    );
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put("/:id", (req, res) => {
+  Category.update(req.body, {
+    where: { id: req.params.id },
+  })
+    .then((category) => {
+      res.status(200).json(category);
+    })
+    .catch((err) =>
+      res.status(500).json({ "message: Internal server error": err })
+    );
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete("/:id", (req, res) => {
+  Category.destroy({
+    where: { id: req.params.id },
+  })
+    .then((category) => {
+      res.status(200).json(category);
+    })
+    .catch((err) =>
+      res.status(500).json({ "message: Internal server error": err })
+    );
 });
 
 module.exports = router;
